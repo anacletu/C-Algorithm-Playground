83043888c5170f8f49bd0f2f2e5499900688a9df
Eric <eric.anacleto@gmail.com>
2023-11-27 14:09:05 +0100

    Implementation of Merge Sort

---
+++ sorting/merge_sort.h
@@ -0,0 +1,11 @@
+#ifndef MERGE_SORT_H
+#define MERGE_SORT_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+// Function declaration for Merge Sort
+void mergeSort(int arr[], size_t length);
+void merge(int arr[], int arrL[], int arrR[], size_t nL, size_t nR);
+
+#endif // MERGE_SORT_H
---
+++ tests/sorting_lib/manual_test_merge_sort.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include "../../sorting/merge_sort.h"
+
+void printArray(int array[], size_t size, const char *label)
+{
+    printf("%s:\n", label);
+    for (int i = 0; i < size; i++)
+    {
+        printf("%i ", array[i]);
+    }
+    printf("\n\n");
+}
+
+int main()
+{
+    // Test case 1: Regular array
+    int array1[] = {64, 25, 12, 22, 11, 1, 0, 48, 100};
+    size_t size1 = sizeof(array1) / sizeof(array1[0]);
+
+    printArray(array1, size1, "Original Array");
+    mergeSort(array1, size1);
+    printArray(array1, size1, "Sorted Array");
+
+    // Test case 2: Empty array
+    int emptyArray[] = {};
+    size_t sizeEmpty = sizeof(emptyArray) / sizeof(emptyArray[0]);
+
+    printArray(emptyArray, sizeEmpty, "Empty Array");
+    mergeSort(emptyArray, sizeEmpty);
+    printArray(emptyArray, sizeEmpty, "Sorted Empty Array");
+
+    // Test case 3: Array with a single element
+    int singleElementArray[] = {42};
+    size_t sizeSingle = sizeof(singleElementArray) / sizeof(singleElementArray[0]);
+
+    printArray(singleElementArray, sizeSingle, "Single Element Array");
+    mergeSort(singleElementArray, sizeSingle);
+    printArray(singleElementArray, sizeSingle, "Sorted Single Element Array");
+
+    // Test case 4: Already sorted array
+    int sortedArray[] = {1, 2, 3, 4, 5};
+    size_t sizeSorted = sizeof(sortedArray) / sizeof(sortedArray[0]);
+
+    printArray(sortedArray, sizeSorted, "Already Sorted Array");
+    mergeSort(sortedArray, sizeSorted);
+    printArray(sortedArray, sizeSorted, "Sorted Already Sorted Array");
+
+    // Test case 5: Array with duplicate elements
+    int duplicateArray[] = {3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5};
+    size_t sizeDuplicate = sizeof(duplicateArray) / sizeof(duplicateArray[0]);
+
+    printArray(duplicateArray, sizeDuplicate, "Array with Duplicates");
+    mergeSort(duplicateArray, sizeDuplicate);
+    printArray(duplicateArray, sizeDuplicate, "Sorted Array with Duplicates");
+
+    return 0;
+}
